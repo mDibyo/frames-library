dce83264f0b36b817a6cc701014e726b0a21bf1f
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2015-11-21 11:57:37 -0800

    Refactor main to allow more extensibility

--- main.cpp
+++ main.cpp
@@ -9,26 +9,31 @@ void sigint_handler(int s) {
   program_shutdown = true;
 }
 
+void identity_unit(FramesInputter &inputter, FramesOutputter &outputter) {
+  libfreenect2::FrameMap frames;
+  while (!program_shutdown) {
+    if (!inputter.getNextFrame(frames)) {
+      std::cout << "no more frames" << std::endl;
+      break;
+    }
+
+    if (!outputter.putNextFrame(frames)) {
+      std::cout << "could not write out frame" << std::endl;
+    }
+  }
+}
+
+
 int main(int argc, char *argv[]) {
   std::cout << "Starting up" << std::endl;
   signal(SIGINT, sigint_handler);
   program_shutdown = false;
 
+  FramesInputterFromDisk inputter("pendulum/filebase_rgb_");
+  FramesOutputterToDiskPNG outputter("nonpendulum/rgb_");
+
   try {
-    FramesInputterFromDisk inputter("pendulum/filebase_rgb_");
-    FramesOutputterToDiskPNG outputter("nonpendulum/rgb_");
-
-    libfreenect2::FrameMap frames;
-    while (!program_shutdown) {
-      if (!inputter.getNextFrame(frames)) {
-        std::cout << "no more frames" << std::endl;
-        break;
-      }
-
-      if (!outputter.putNextFrame(frames)) {
-        std::cout << "could not write out frame" << std::endl;
-      }
-    }
+    identity_unit(inputter, outputter);
   } catch (FramesIOException& exception) {
     std::cout << "Could not initialize input. " << std::endl;
     return -1;
